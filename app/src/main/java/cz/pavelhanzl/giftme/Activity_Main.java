6f68ebfe5b56a559c5231d3c12ac83810d0e47b1
Pavel Hanzl <pavelhanzl@github.com>
2019-03-18 23:12:31 +0100

    menu prepina fragmenty, logout tlacitko v menu odhlasi prihlaseneho uzivatele

--- app/src/main/java/cz/pavelhanzl/giftme/Activity_Main.java
+++ app/src/main/java/cz/pavelhanzl/giftme/Activity_Main.java
@@ -1,19 +1,31 @@
 package cz.pavelhanzl.giftme;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.LoginFilter;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Activity_Main extends AppCompatActivity {
     //Deklarace členských proměnných
     private DrawerLayout mDrawerLayout;
     private String mStringUserEmail;
     private TextView mTextViewUserEmail;
+    private NavigationView mNavigationView;
+    private FirebaseAuth mAuth;
+    private Intent mIntentLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +39,73 @@ public class Activity_Main extends AppCompatActivity {
         //Link xml a java kódu
         mDrawerLayout = findViewById(R.id.drawer_layout);
         mTextViewUserEmail = findViewById(R.id.nav_textview_user_email);
+        mNavigationView = findViewById(R.id.nav_view);
+
+        //Získá instanci FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        //nastaví Intents
+        mIntentLogin = new Intent(this, Activity_Login.class);
 
         //přidá ikonku pro vyvolání drawer menu do toolbaru
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState(); //synchronizuje animaci ikonky s polohou drawer menu
 
-        //nastaví jméno uživatele v drawer menu
+        //nastavi fragment na giftlist pri prvnim otevreni aktivity - neni predana zadne savedInstanceState
+        if(savedInstanceState == null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Fragment_Giftlist()).commit();
+        mNavigationView.setCheckedItem(R.id.nav_giftlist);}
+
+        //Listenery:
+
+        //řídí navigaci
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_giftlist:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Fragment_Giftlist()).commit();
+                        break;
+                    case R.id.nav_groups:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Fragment_Groups()).commit();
+                        break;
+                    case R.id.nav_stats:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Fragment_Stats()).commit();
+                        break;
+                    case R.id.nav_settings:
+                        //TODO: dodělat settings
+                        Toast.makeText(Activity_Main.this, "settings pressed", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_logout:
+                        LogOut();
+
+                        break;
+                }
+
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        //TODO: nastaví jméno uživatele v drawer menu
+
 
     }
 
+    /**
+     * Odhlásí uživatele z aplikace a zobrazí login
+     */
+    private void LogOut() {
+        //TODO: dodělat "opravdu odhlásit?"
+        mAuth.signOut();
+        startActivity(mIntentLogin);
+        finish();
+    }
+
+    /**
+     * Pokud je otevřené drawer menu, tak se při stisknutí tlačítka BACK zavře.
+     */
     @Override
     public void onBackPressed() {
         if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
